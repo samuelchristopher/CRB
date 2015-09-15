08def441c5b0840edf8f6bb8d95b135c0ed11aca
Samuel Christopher <thesamchris@gmail.com>
2015-09-14 20:45:08 -0700

    Fix: signing in for inactive users

--- app/routes/auth/login.php
+++ app/routes/auth/login.php
@@ -23,9 +23,11 @@ $app->post('/login', $guest(), function()  use ($app) {
 
   if ($v->passes()) {
     $user = $app->user
-    ->where('username', $identifier)
-    ->orWhere('email', $identifier)
     ->where('active', true)
+    ->where(function($query) use ($identifier) {
+      return $query->where('email', $identifier)
+        ->orWhere('username', $identifier);
+    })
     ->first();
 
     if ($user && $app->hash->passwordCheck($password, $user->password)) {
