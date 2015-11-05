3564c3783ee7915a7bf8a8d6d6d1398bbf50868c
Samuel Christopher <thesamchris@gmail.com>
2015-11-05 08:39:11 +0000

    Added desktop nac

--- app/views/dev/templates/default.php
+++ app/views/dev/templates/default.php
@@ -2,9 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="./app/views/dev/src/nav.css" media="screen" title="no title" charset="utf-8">
     <title>{% block title %}{% endblock %}</title>
   </head>
   <body style="font-family: sans-serif">
+    {% include 'dev/templates/partials/nav.php' %}
     {% block content %}{% endblock %}
   </body>
 </html>
---
+++ app/views/dev/templates/partials/nav.php
@@ -0,0 +1,9 @@
+  <header class="dev-header">
+    <div class="container">
+      <div class="row">
+        <a href="#" class="header-logo ">Logo</a>
+        <a href="#" class="header-getstarted button button-filled">Get Started Free!</a>
+        <a href="#login" class="header-login button button-outline">Log in</a>
+      </div>
+     </div>
+  </header>
