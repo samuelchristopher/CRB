0ed323543ec5edcebfa940c1a496c139a3ab968c
Samuel Christopher <thesamchris@gmail.com>
2016-01-22 05:24:52 +0000

    Final v1

--- app/views/home.php
+++ app/views/home.php
@@ -9,10 +9,18 @@
       nav.navbar.navbar-default {
         margin: 0;
       }
+
+      .carousel-caption h1 {
+        color: white;
+      }
+
+      .carousel-caption p {
+        color: rgb(226, 226, 226);
+      }
   </style>
     <!-- Carousel
     ================================================== -->
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin-bottom: 0">
     <!-- Indicators -->
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -20,7 +28,7 @@
       <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner" role="listbox">
-      <div class="item active" style="background: url(https://images.unsplash.com/photo-1429497419816-9ca5cfb4571a?q=80&fm=jpg&s=4bf1164d23eea4f04aeefe1732149cf3) no-repeat;">
+      <div class="item active" style="background: url(app/assets/one.jpg) no-repeat;background-size: cover;background-position: center center;">
         <!-- <img class="first-slide" src="https://images.unsplash.com/photo-1429497419816-9ca5cfb4571a?q=80&fm=jpg&s=4bf1164d23eea4f04aeefe1732149cf3" alt="First slide"> -->
         <div class="container">
           <div class="carousel-caption">
@@ -30,8 +38,8 @@
           </div>
         </div>
       </div>
-      <div class="item">
-        <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+      <div class="item" style="background: url(app/assets/two.jpg) no-repeat;background-size: cover;background-position: center center;">
+        <!-- <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide"> -->
         <div class="container">
           <div class="carousel-caption">
             <h1>Another example headline.</h1>
@@ -40,8 +48,8 @@
           </div>
         </div>
       </div>
-      <div class="item">
-        <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+      <div class="item" style="background: url(app/assets/three.jpg) no-repeat;background-size: cover;background-position: center center;">
+        <!-- <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide"> -->
         <div class="container">
           <div class="carousel-caption">
             <h1>One more for good measure.</h1>
@@ -68,30 +76,29 @@
 
   <div class="container marketing" id="#market">
 
-    <h1 style="text-align:center;">Previous clients</h1>
+    <!-- <h1 style="text-align:center;">Previous clients</h1>
     <hr>
 
-    <!-- Three columns of text below the carousel -->
     <div class="row">
       <div class="col-lg-4">
         <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
         <h2>Shell</h2>
         <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
         <p><a class="btn btn-default" href="{{ urlFor('company.clients') }}" role="button">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
+      </div>
       <div class="col-lg-4">
         <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
         <h2>Heading</h2>
         <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
         <p><a class="btn btn-default" href="{{ urlFor('company.clients') }}" role="button">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
+      </div>
+      <div-4">
         <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
         <h2>Heading</h2>
         <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
         <p><a class="btn btn-default" href="{{ urlFor('company.clients') }}" role="button">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+      </div>
+    </div> -->
 
 
     <!-- START THE FEATURETTES -->
@@ -104,7 +111,7 @@
         <p class="lead">Gone are the days where you would have to physically wait in line for certification. Our client management system here at {{ appName }} enables you to get certified online. Does that interest you? <a href="{{ urlFor('register') }}">Register now</a></p>
       </div>
       <div class="col-md-5">
-        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        <img class="featurette-image img-responsive center-block" src="app/assets/four.jpg" alt="Generic placeholder image">
       </div>
     </div>
 
@@ -116,7 +123,7 @@
         <p class="lead">Never again do you have to lock your certificates up in a safe. By registering with us, you allow us to strip away all the cons of certification. Basically, we got your <s>back</s> certificates!</p>
       </div>
       <div class="col-md-5 col-md-pull-7">
-        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        <img class="featurette-image img-responsive center-block" src="app/assets/five.jpg" alt="Generic placeholder image">
       </div>
     </div>
 
@@ -128,7 +135,7 @@
         <p class="lead">When you register with us, you will be granted access to viewing the rules and guidelines of certification for your respective country. This is the deal maker for most of our clients. Still not convinced?  Have a look at our <a href="{{ urlFor('company.clients') }}">previous clients.</a></p>
       </div>
       <div class="col-md-5">
-        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        <img class="featurette-image img-responsive center-block" src="app/assets/six.png" alt="Generic placeholder image">
       </div>
     </div>
 
