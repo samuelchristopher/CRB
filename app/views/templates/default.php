c9a4be65d81f086926d224cb1c2dc3eb54af7e4b
Samuel Christopher <thesamchris@gmail.com>
2016-07-12 21:31:49 +0800

    Fixed container

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -11,6 +11,11 @@
     <!-- Optional theme -->
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
     <link rel="stylesheet" href="https://bootswatch.com/lumen/bootstrap.min.css">
+    <style>
+      .container {
+        margin: 20px;
+      }
+    </style>
   </head>
   <body>
     {% include 'templates/partials/messages.php' %}
